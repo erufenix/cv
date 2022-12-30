ac337d59a5bcd53b46c18cd52e4acb5066ae24e0
Erufenix <erufenix@gmail.com>
2022-12-30 16:51:16 -0600

    opengrah include

--- src/App/Routes.php
+++ src/App/Routes.php
@@ -5,5 +5,5 @@ declare(strict_types=1);
 //$app->get('/', 'App\Controller\Home:getHelp');
 //$app->get('/status', 'App\Controller\Home:getStatus');
 
-$app->get('/', 'App\Controller\Home:esCV');
-$app->get('/en', 'App\Controller\Home:enCV');
+$app->get('/', 'App\Controller\Home:esCV')->setName('cv_es');;
+$app->get('/en', 'App\Controller\Home:enCV')->setName('cv_en');;
